da8e8508edf7e604f80d80d8841c10ae4ac9eef0
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-10 16:48:33 -0800

    created new files to send tweet from python

---
+++ markovtweets.py
@@ -0,0 +1,15 @@
+import os
+
+import twitter
+
+api = twitter.Api(
+    consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+    consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+    access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+)
+
+print api.VerifyCredentials()
+
+status = api.PostUpdate("Test")
+print status.text
